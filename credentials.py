f284543a05564ff3a76f712f66ee885c613b9f05
zebulon0 <zah1276@rit.edu>
2020-09-17 13:52:37 -0400

    current version

--- credentials.py
+++ credentials.py
@@ -1,6 +1,12 @@
-#  File that hides secret keys to be used to access
-#  the twitter API
+"""
+ File that hides secret keys to be used to access
+ the twitter API and giphy API
+"""
+#  for twitter API
 consumer_key = 'Easd5anvJicTU7MTtfOOiYGTe'
 consumer_secret = 'o5xj43y0ZXpGkOeYekMJPMrDAB9ZudWQywdZtUAycRlLhsZSFu'
 access_token = '1305264713081720835-pjfcJHFMlQSdnLXIpVmvXlQMItJnW6'
 access_token_secret = 'TfUGaDaAauNrdj1Oea2rET4nXe99ccj0ofFCf4Yx7KI7X'
+
+#  for giphy API
+YOUR_API_KEY = 'Qos2ut9wu6XBN2woR6LmNkb9uS46e7QV'
--- twitter-bot.py
+++ twitter-bot.py
@@ -1,14 +1,78 @@
+import requests
 import tweepy
+import giphy_client
 from credentials import *
+import json
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
 
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#     print(tweet.text)
+def init_tweepy():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    return tweepy.API(auth)
 
-# Write a tweet to push to our Twitter account
-tweet = 'Hello, world!'
-api.update_status(status=tweet)
+
+def init_giphy():
+    # create an instance of the API class
+    api_instance = giphy_client.DefaultApi()
+    api_key = 'Qos2ut9wu6XBN2woR6LmNkb9uS46e7QV'  # str | Giphy API Key.
+    q = 'puppy'  # str | Search query term or phrase.
+    limit = 1  # int | The maximum number of records to return. (optional) (default to 25)
+    offset = 0  # int | An optional results offset. Defaults to 0. (optional) (default to 0)
+    rating = 'g'  # str | Filters results by specified rating. (optional)
+    lang = 'en'  # str | Specify default country for regional content; use a 2-letter ISO 639-1 country code. See list of supported languages <a href = \"../language-support\">here</a>. (optional)
+    fmt = 'json'  # str | Used to indicate the expected response format. Default is Json. (optional) (default to json)
+    api_response = api_instance.gifs_search_get(api_key, q, limit=limit, offset=offset, rating=rating, lang=lang, fmt=fmt)
+    print(api_response.data[0])
+    print(json.dump(api_response.data[0], ))
+
+
+
+
+# data=json.loads(urllib("http://api.giphy.com/v1/gifs/search?q=puppy&api_key=" + YOUR_API_KEY + "&limit=5").read())
+# json.dumps(data, sort_keys=True, indent=4)
+
+def gif_post(gif_url_list, msg):
+    """
+    tweets GIFs and sleeps for a specific time
+    """
+    for i in range(len(gif_url_list)):
+        try:
+            gif_download(gif_url_list[i])
+            m = modifier(msg[i])
+            tweet(m)
+        except:
+            continue
+
+
+def tweet(tweet_msg):
+    api = init_tweepy()
+    message = tweet_msg + " #funny #gif #lol #humor"  # TODO: Add desired tweet message here
+    api.update_with_media('image.gif', status=message)
+
+
+def modifier(s):
+    '''
+    returns hashtags based on the GIF names from GIPHY
+    '''
+    ms = ''
+    for i in range(len(s)):
+        if s[i] == '-':
+            ms += ' '
+        else:
+            ms += s[i]
+    ls = ms.split()
+    del ls[-1]
+    ls[0] = "#" + ls[0]
+    return " #".join(ls)
+
+
+def gif_download(gif_url):
+    '''
+    Takes the URL of an Image/GIF and downloads it
+    '''
+    gif_data = requests.get(gif_url).content
+    with open('image.gif', 'wb') as handler:
+        handler.write(gif_data)
+        handler.close()
+
+init_giphy()
